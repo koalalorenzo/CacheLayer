ee77a65a0abdcdf566b90605c17ff0c48942c1ad
Lorenzo Setale <koalalorenzo@gmail.com>
2015-03-08 20:02:13 +0100

    Connecting the properties to the API available and hiding some values

--- services/api.py
+++ services/api.py
@@ -1,8 +1,14 @@
 from tastypie.resources import ModelResource
+from tastypie import fields
+
 from .models import Service
 
 class ServiceResource(ModelResource):
+    is_down = fields.BooleanField(attribute='is_down')
+    endpoint = fields.CharField(attribute='endpoint')
+
     class Meta:
         queryset = Service.objects.all()
         resource_name = 'service'
         allowed_methods = ['get']
+        excludes = ['force_down', 'is_crawler_enabled', 'created_at', 'edited_at']
