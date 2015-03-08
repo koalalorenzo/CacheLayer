a8a4416c32854d59dd3a3d86717723bfe4fe3d1b
Lorenzo Setale <koalalorenzo@gmail.com>
2015-03-08 20:01:31 +0100

    Added request in templates

--- opendatacache/settings/default.py
+++ opendatacache/settings/default.py
@@ -23,8 +23,20 @@ SECRET_KEY = '-=eemw)z^!#g0ez1$9+r#(+2jmu$x4b5)yh@04t^z9+cz!ilw^'
 DEBUG = False
 
 TEMPLATE_DEBUG = False
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, "templates"),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
 )
 
 ALLOWED_HOSTS = []
