19c69258daca788d789ae003f2497d0dff0b1889
Lorenzo Setale <koalalorenzo@gmail.com>
2015-03-11 18:20:07 +0100

    Code more readable

--- services/views.py
+++ services/views.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import logging
 logger = logging.getLogger(__name__)
 
-from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import render
@@ -22,28 +21,33 @@ HOP_BY_HOP_HEADERS = [
     "upgrade",
 ]
 
+
 def get_data(request, domain, extra_url=None):
     """"""
 
     service = get_object_or_404(Service, domain=domain)
-    
+
     content = service.get_content(request)
     if not content:
-        response = HttpResponseServerError("Service not available or cache expired")
+        response = HttpResponseServerError(
+            "Service not available or cache expired"
+        )
         response["X-CacheLayer-Created"] = datetime.now()
 
     else:
-        response = HttpResponse(content["content"], content_type=content["content-type"])
-        
+        response = HttpResponse(
+            content["content"], content_type=content["content-type"]
+        )
+
         for header_key, header_value in content["headers"].iteritems():
             if header_key.lower() in HOP_BY_HOP_HEADERS:
                 continue
             response[header_key] = header_value
-        
         response["X-CacheLayer-Created"] = content['created']
-        
-    response["X-CacheLayer-Status"] = "HIT" if service.is_down else "LIVE"
+
+    response["X-CacheLayer-Status"] = "HIT" if service.is_down else "GOT"
     return response
+
 
 def service_list(request):
     return render(request, 'services-list.html')
