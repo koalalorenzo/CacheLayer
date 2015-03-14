b00125d0a7d544a28d287e6857b8bce199992b85
Lorenzo Setale <koalalorenzo@gmail.com>
2015-03-14 08:56:19 +0100

    Small refactoring of the code to make it readable.

--- cachelayer/urls.py
+++ cachelayer/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = patterns(
+    '',
     url(r'^', include('services.urls')),
     url(r'^robots.txt$', 'cachelayer.views.robots'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += patterns('django.contrib.staticfiles.views',
-	url(r'^static/(?P<path>.*)$', 'serve'),
+# Servering static files too
+urlpatterns += patterns(
+    'django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve'),
 )
