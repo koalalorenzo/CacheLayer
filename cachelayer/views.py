76e9d69c1420060a4bd3a629ae973a094fce23ec
Lorenzo Setale <koalalorenzo@gmail.com>
2015-03-21 10:37:02 +0100

    Unified migrations again.

--- cachelayer/views.py
+++ cachelayer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+
 def robots(request):
     return render(request, 'robots.txt')
--- services/migrations/0001_initial.py
+++ services/migrations/0001_initial.py
@@ -2,31 +2,34 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Service',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=50, verbose_name=b'Name')),
-                ('domain', models.CharField(max_length=60, null=True, verbose_name=b'Domain', blank=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(max_length=50, verbose_name='Name')),
+                ('domain', models.CharField(max_length=60, verbose_name='Domain')),
                 ('descrition', models.TextField(null=True, blank=True)),
-                ('store_days', models.IntegerField(default=7, verbose_name=b'Days to keep data')),
-                ('check_period', models.IntegerField(default=60, verbose_name=b'Period of downtime')),
-                ('request_timeout', models.IntegerField(default=30, verbose_name=b'Request timeout')),
-                ('cache_duration', models.IntegerField(default=60, verbose_name=b'Cache expiration')),
+                ('store_days', models.IntegerField(verbose_name='Days to keep data', default=7)),
+                ('check_period', models.IntegerField(verbose_name='Period of downtime', default=60)),
+                ('request_timeout', models.IntegerField(verbose_name='Request timeout', default=30)),
+                ('cache_duration', models.IntegerField(verbose_name='Cache expiration', default=60)),
                 ('is_enabled', models.BooleanField(default=False)),
                 ('force_down', models.BooleanField(default=False)),
                 ('is_crawler_enabled', models.BooleanField(default=False)),
                 ('crawled_at', models.DateTimeField(null=True, blank=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('edited_at', models.DateTimeField(auto_now=True)),
+                ('created_by', models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, null=True)),
             ],
             options={
                 'verbose_name': 'Service',
