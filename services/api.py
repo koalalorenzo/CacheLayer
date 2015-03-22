e6ccfcf6e8d820130ae29500e48cc19dd1ce6a77
Lorenzo Setale <koalalorenzo@gmail.com>
2015-03-22 16:54:55 +0100

    Disabled authentication/authorization for Service API

--- services/api.py
+++ services/api.py
@@ -1,12 +1,14 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
+from tastypie.authentication import Authentication
 from tastypie import fields
 
 from .models import Service
 
 
 class ServiceResource(ModelResource):
-    is_down = fields.BooleanField(attribute='is_down')
-    endpoint = fields.CharField(attribute='endpoint')
+    is_down = fields.BooleanField(attribute='is_down', readonly=True)
+    endpoint = fields.CharField(attribute='endpoint', readonly=True)
 
     class Meta:
         queryset = Service.objects.all()
@@ -16,3 +18,6 @@ class ServiceResource(ModelResource):
             'force_down', 'is_crawler_enabled',
             'created_at', 'edited_at'
         ]
+        authentication = Authentication()
+        authorization = Authorization()
+        always_return_data = True
