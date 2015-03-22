74e991bc106a6945d7e9b3adf5f25dd8dd8a18d9
Lorenzo Setale <koalalorenzo@gmail.com>
2015-03-22 19:29:49 +0100

    Now we can also delete Services from the front-end using Angular and cute cats!

--- static/js/app.js
+++ static/js/app.js
@@ -21,6 +21,11 @@
                     controller: 'editServiceController',
                 })
 
+                .when('/service/:id/delete/', {
+                    templateUrl: '/static/angular/delete.html',
+                    controller: 'deleteServiceController',
+                })
+
                 .when('/services/new', {
                     templateUrl: '/static/angular/edit.html',
                     controller: 'editServiceController',
@@ -37,7 +42,7 @@
 
         }]);
 
-    app.controller('servicesController', ['$http', '$scope', '$timeout', '$routeParams', function($http, $scope, $timeout, $routeParams){
+    app.controller('servicesController', ['$http', '$scope', '$location', '$timeout', '$routeParams', function($http, $scope, $location, $timeout, $routeParams){
         $scope.services = [];
         $scope.is_loading = true;
 
@@ -77,13 +82,13 @@
             $http.get("/api/v1/service/"+$scope.service.id+"/?format=json")
 
                 .success(function(data){
-
                     $scope.service = data;
                     $scope.is_loading = false;
                 })
 
                 .error(function(data){
                     $scope.is_loading = false;
+                    console.log(data);
                     alert("Unable to refresh. Try again later.");
                 });
 
@@ -95,10 +100,18 @@
             entry.request_timeout = parseInt(entry.request_timeout,10);
             entry.cache_duration = parseInt(entry.cache_duration,10);
 
-            $http.post($scope.service_url, entry)
+            var call_method = 'POST';
+            if($scope.service.id)
+                call_method = 'PUT';
+
+            var req = {
+                method: call_method,
+                url: $scope.service_url,
+                data: entry,
+            }
+
+            $http(req)
                 .success(function(data){
-                    console.log(data);
-                    alert("Saved");
                     $scope.reset();
                     $location.path('service/'+data.id);
                 })
@@ -114,7 +127,7 @@
 
     }]);
 
-    app.controller('serviceController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams){
+    app.controller('serviceController', ['$http', '$scope', '$location', '$routeParams', function($http, $scope, $location, $routeParams){
         $scope.service = {};
         $scope.is_loading = true;
 
@@ -125,18 +138,62 @@
             $http.get("/api/v1/service/"+service_id+"/?format=json")
 
                 .success(function(data){
-
                     $scope.service = data;
                     $scope.is_loading = false;
                 })
 
                 .error(function(data){
                     $scope.is_loading = false;
+                    console.log(data);
                     alert("Unable to refresh. Try again later.");
+                    $location.path("404");
                 });
         };
 
         $scope.get_service($routeParams.id);
 
     }]);
+
+    app.controller('deleteServiceController', ['$http', '$scope', '$location', '$routeParams', function($http, $scope, $location, $routeParams){
+        $scope.service = {};
+        $scope.is_loading = true;
+
+        $scope.get_service = function(service_id){
+            $scope.service = {};
+            $scope.is_loading = true;
+
+            $http.get("/api/v1/service/"+service_id+"/?format=json")
+
+                .success(function(data){
+                    $scope.service = data;
+                    $scope.is_loading = false;
+                })
+
+                .error(function(data){
+                    $scope.is_loading = false;
+                    console.log(data);
+                    alert("Unable to refresh. Try again later.");
+                    $location.path("404");
+                });
+        };
+
+        $scope.delete = function(service){
+            $http.delete("/api/v1/service/"+$scope.service.id+"/?format=json")
+                .success(function(data){
+                    $scope.is_loading = false;
+                    alert("Deleted :(");
+                    $location.path("services");
+                })
+                .error(function(data){
+                    $scope.is_loading = false;
+                    console.log(data);
+                    alert("Unable to refresh. Try again later.");
+                    $location.path("services");
+                });
+        };
+
+        $scope.get_service($routeParams.id);
+    }]);
+
+
 })();
